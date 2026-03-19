04e316eda43081885922e0e29aabcc55cf1fe0ff
Quinn Fibin <qfilbin@gmail.com>
2026-03-18 21:03:27 -0500

    Deploying

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Button } from 'bootstrap'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return <div>
@@ -13,4 +13,4 @@ function App() {
   
 }
 
-export default App
+
