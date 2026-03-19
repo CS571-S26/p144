adf9d0c9bbf52a04e227955256eb17d78b73b0f6
Quinn Fibin <qfilbin@gmail.com>
2026-03-18 20:53:11 -0500

    Fresh restart of project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from 'bootstrap'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div>
+    <h1>
+      Welcome to my Project!
+    </h1>
+  </div>
+  
+}
+
+export default App
